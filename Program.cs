9c1abe1fa580679e38989487eafa595872aff3e4
Wonder Oladunjoye <itz.wondee@gmail.com>
2024-11-03 15:41:42 -0600

    Sunday's Assignment.

--- Program.cs
+++ Program.cs
@@ -9,59 +9,35 @@ namespace Console_Application
             // Asking the user to input te date.
             Console.WriteLine("What day is it today?" + "\n");
 
-            // Getting the current date from the datetime object.
-            DayOfWeek dow = DateTime.Now.DayOfWeek;
-
             //Using switch case to display the correct date.
-            switch (dow)
+            switch (DateTime.Now.DayOfWeek)
                 {
                     case DayOfWeek.Monday:
-                    string M = "Monday";
-                    string upcase = M.ToUpper();
-                    string day = $"Today is: {upcase}";
-                    Console.WriteLine(day + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Monday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Tuesday:
-                    string T = "Tuesday";
-                    string u = T.ToUpper();
-                    string d = $"Today is: {u}";
-                    Console.WriteLine(d + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Tuesday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Wednesday:
-                    string W = "Wednesday";
-                    string up = W.ToUpper();
-                    string da = $"Today is: {up}";
-                    Console.WriteLine(da + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Wednesday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Thursday:
-                    string TH = "Thursday";
-                    string upc = TH.ToUpper();
-                    string ad = $"Today is: {upc}";
-                    Console.WriteLine(ad + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Thursday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Friday:
-                    string F = "Friday";
-                    string upca = F.ToUpper();
-                    string fr = $"Today is: {upca}";
-                    Console.WriteLine(fr + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Friday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Saturday:
-                    string S = "Saturday";
-                    string upcas = S.ToUpper();
-                    string sa = $"Today is: {upcas}";
-                    Console.WriteLine(sa + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Saturday.ToString().ToUpper()}" + "\n");
                     break;
 
                     case DayOfWeek.Sunday:
-                    string SU = "Sunday";
-                    string upcased = SU.ToUpper();
-                    string sun = $"Today is: {upcased}";
-                    Console.WriteLine(sun + "\n");
+                    Console.WriteLine($"Today is: {DayOfWeek.Sunday.ToString().ToUpper()}" + "\n");
                     break;
             }
 
