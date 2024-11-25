2f58a2f65289ef3b38f9507bdfec30e1a2a32322
Wonder Oladunjoye <itz.wondee@gmail.com>
2024-11-25 13:20:14 -0600

    Work in progress

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Console_Application
+{
+    public class Car
+    {
+        public string model;
+        public string color; // Ensure this property exists
+        public int year;
+
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,114 +1,36 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Globalization;
 
-namespace Console_Application
+namespace Console_Application // Match the namespace of both files
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            bool isRunning = true;
-            while (isRunning)
-            {
-                // We are asking the user to choose an option.
-                Console.WriteLine("Choose between options 1, 2, 3, and 4.\n");
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red"; // This line uses the 'color' property
+            Ford.year = 1969;
 
-                // Making sure the user can only input interger values.
-                if (!int.TryParse(Console.ReadLine(), out int userOptions))
-                {
-                    Console.WriteLine("Invalid input. Please choose between the given options.");
-                    continue;
-                }
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white"; // This line uses the 'color' property
+            Opel.year = 2005;
 
-                // Using switch case to output the correct option
-                switch (userOptions)
-                {
-                    case 1:
-                        // Request the user to input data.
-                        Console.WriteLine("\nInput a word or sentence.\n");
+            Console.WriteLine("The " + Ford.model + " is " + Ford.color + " and it was made in the year " + Ford.year);
+            Console.WriteLine("The " + Opel.model + " is " + Opel.color + " and it was made in the year " + Opel.year + "\n");
 
-                        // The data is stored in input.
-                        string input = Console.ReadLine();
-
-                        // Giving the user options on casing.
-                        Console.WriteLine("\nDo you want your input to be upper or lower case?");
-                        Console.WriteLine("Indicate by typing (u) for upper or (l) for lower.\n");
-
-                        // The users option is stored in "options".
-                        string options = Console.ReadLine()/*?.ToLower()*/;
-
-                        // If the option is "u" uppercase, else lowercase.
-                        if (options == "u")
-                        {
-                            Console.WriteLine(input.ToUpper() + "\n");
-                        }
-                        else if (options == "l")
-                        {
-                            Console.WriteLine(input.ToLower() + "\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine(options + " is not a valid option!");
-                        }
-                        break;
-
-                    case 2:
-                        Console.WriteLine($"\nToday is {DateTime.Today:D}. It is a beautiful {DateTime.Now.DayOfWeek}.\n");
-                        break;
-
-                    case 3:
-                        // Requesting the user's input for the first number.
-                        Console.WriteLine("\nPlease enter a number.");
-                        // The data is stored in "num1".
-                        if (!int.TryParse(Console.ReadLine(), out int num1))
-                        {
-                            Console.WriteLine("Invalid input. Please enter a number.");
-                            break;
-                        }
-
-                        // Requesting the user's input for the second number.
-                        Console.WriteLine("Please enter another number.");
-                        // The data is stored in "num2".
-                        if (!int.TryParse(Console.ReadLine(), out int num2))
-                        {
-                            Console.WriteLine("Invalid input. Please enter a number.");
-                            break;
-                        }
-
-                        // Requesting the user's input for the arithmetic operator.
-                        Console.WriteLine("Please enter an arithmetic operator, either + for addition or - for subtraction.");
-                        // The operator is stored in "arithmetic".
-                        string arithmetic = Console.ReadLine();
-
-                        // Based on the operator input, we will perform addition or subtraction.
-                        if (arithmetic == "+")
-                        {
-                            Console.WriteLine($"The result of adding {num1} and {num2} is {num1 + num2}.\n");
-                        }
-                        else if (arithmetic == "-")
-                        {
-                            Console.WriteLine($"The result of subtracting {num1} and {num2} is {num1 - num2}.\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine(arithmetic + " is not a valid operator! Please enter either + or -.");
-                        }
-                        break;
-
-                    case 4:
-                        Console.WriteLine("\nExiting the program. Sayonara!");
-                        isRunning = false;
-                        break;
-
-                    default:
-                        Console.WriteLine(userOptions + " is not a valid option!");
-                        break;
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("Press any key to terminate");
-            Console.ReadKey();
+            Ford.fullThrottle();
+            Console.ReadLine();
         }
     }
 }
+
+// 2 players. 20 Cardsc 1 Deck. 
+// Using OOP
+// Beginning of every round shuffle cards. Each player gets 10 cards each.
+// Cards numbered 1-20, when cards shuffled, the numbers change.
+// Each player does the sum of their first five cards
+// Any players sum greater than 30 will receive a score of zero for that round, if less than 30, the sum is their score.
+// Five rounds.
+// After five rouunds, player with the highest score wins.
+// Let's Go!!
